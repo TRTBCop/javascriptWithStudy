42ccaeeac9f0242475ee502fbe6ed27e80d002c4
kimsj912 <aa991122aa@gmail.comn>
2021-11-25 17:30:38 +0900

    계산기 클립보드 CRD 완성

--- Kim_Sujeong/계산기(JS)/calc.js
+++ Kim_Sujeong/계산기(JS)/calc.js
@@ -1,72 +1,96 @@
 // Constant
+const baiscOps = ['+','-','*','/'];
 const scienCalcOps = ["+-","mod","exp","x^2","1/x","10^x","sin","cos","tan"];
 
 // Dom Element
 const textInput = document.querySelector("input[type='text']");
 const buttonInput = document.querySelectorAll("form input[type='button']");
-const openClipboard = document.querySelector(".button-box input[value='클립보드']");
-const clipboard = document.querySelector(
-    ".clipboard");
+const openClipboardBtn = document.querySelector(".button-box input[value='클립보드']");
+const clipboard = document.querySelector(".clipboard");
 const clipUl = document.querySelector('.clipboard ul');
 
-const openscienceCalc = document.querySelector(".button-box input[value='공학계산기']");
+const openscienceCalcBtn = document.querySelector(".button-box input[value='공학계산기']");
 const scientificCalc = document.querySelector(".scientificCalc");
 const scForm = document.querySelector(".scientificCalc form");
 
 // Variable
 let clip = [];
 
-// 여러번 쓰기 귀찮아서 이벤트 다는 거 한번에 모음.
-const size = buttonInput.length;
-Array(size).fill().forEach((_,i)=>{
+// 버튼 별 이벤트 설정
+Array(buttonInput.length).fill().forEach((_,i)=>{
     const element = buttonInput[i];
     if(element.value==='C') {
         element.addEventListener('click',()=>textInput.value='');
         element.addEventListener('dblclick',()=>{
-            // logic
             clip = [];
-            // view 
-            drawClipboard();
+            showClipboard();
         });
     } else if(element.value==='=') {
         element.addEventListener('click',()=>{
-            // logic
-            clip.push(textInput.value);
-            // view
-            textInput.value=calculator(textInput.value);
-            drawClipboard();
+            if(textInput.value!=='') {
+                let justCameIn = textInput.value;
+                let calculated = calculate(justCameIn);
+
+                textInput.placeholder = calculated;
+                textInput.value = '';
+                
+                addClipboard(justCameIn, calculated);
+            }
         });
     } else if (element.value === 'del'){
         element.addEventListener('click',()=> textInput.value = textInput.value.slice(0, -1));
-    } else 
+    } else {
         element.addEventListener('click',()=>textInput.value+=element.value);
+    }
 })
 
 // Function
-function calculator(expression){
+function calculate(expression){
     return eval(expression);
 }
 
-function drawClipboard(){
-    clipUl.innerHTML = '';
+function addClipboard(justCameIn,calculated){
+    const obj = {
+        id : new Date(),
+        text  : `${justCameIn} = ${calculated}`,
+    }
+    clip.push(obj);
+    showClipboard();
+}
+
+function deleteClipboard(e){
+    const li = e.target.parentElement;
+    clip = clip.filter((c)=>c.id!=li.id);
+    showClipboard();
+}
+
+function showClipboard(){
     if(clip.length===0) {
+        clipUl.innerHTML = '';
         const li = document.createElement('li');
         li.innerText = `아직 계산 결과가 존재하지 않습니다.`;
         clipUl.appendChild(li);
-    }
-    else {
+    } else{
+        clipUl.innerHTML = '';
+        console.log(clip.length);
         clip.forEach(element => {
-        const li = document.createElement('li');
-        li.innerText = `${element} = ${calculator(element)}`;
-        li.addEventListener('dblclick',()=> textInput.value=element);
-        clipUl.appendChild(li);
+            const li = document.createElement('li');
+            const span = document.createElement("span");
+            const button = document.createElement("button");
+        
+            li.id = element.id;
+            button.innerText = "❌";
+            span.innerText = element.text;
+            button.addEventListener("click",deleteClipboard);
+        
+            li.appendChild(button);
+            li.appendChild(span);
+            li.addEventListener('dblclick',()=> textInput.value=element.slice(0,element.indexOf('=')-1));
+            clipUl.appendChild(li);
         });
     }
 }
 
 // Event
-openscienceCalc.addEventListener('click', ()=> scientificCalc.classList.toggle('hidden'));
-openClipboard.addEventListener('click', ()=> {
-    drawClipboard();
-    clipboard.classList.toggle('hidden');
-});
+openscienceCalcBtn.addEventListener('click', ()=> scientificCalc.classList.toggle('hidden'));
+openClipboardBtn.addEventListener('click', ()=>clipboard.classList.toggle('hidden'));
