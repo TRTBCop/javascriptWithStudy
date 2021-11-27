4d9bacd0e65790a98c3051f70fb2cc9b712ec5ec
윤예은 <60867063+yeeun-yun97@users.noreply.github.com>
2021-11-27 23:58:33 +0900

    w05 call 119

--- Yoon_Yeeun/[5주차] 계산기/script.js
+++ Yoon_Yeeun/[5주차] 계산기/script.js
@@ -5,9 +5,13 @@ const INPUTS = document.querySelectorAll("div form input[type=button]")
 const MEMORY_BUTTON = document.querySelector("div form input.memory[value='M']")
 const RECALL_BUTTON = document.querySelector("div form input.memory[value='R']")
 let memoryValue = ''
+let hasDots=[false];
 
 const CLEAR_BUTTON = document.querySelector("div form input[type=button][value='C']")
 const CLEAR_ONE_BUTTON = document.querySelector("div form input[type=button].clear.one")
+
+const DOT_BUTTON = document.querySelector("div form input.number[value='.']")
+const CALC_BUTTON = document.querySelector("div form input[value='=']")
 
 clear()
 
@@ -22,6 +26,9 @@ MEMORY_BUTTON.setAttribute('onclick', 'memory()')
 RECALL_BUTTON.setAttribute('onclick', 'recall()')
 CLEAR_BUTTON.setAttribute('onclick', 'clear()')
 CLEAR_ONE_BUTTON.setAttribute('onclick', 'erase()')
+DOT_BUTTON.setAttribute('onclick', 'dot()')
+CALC_BUTTON.setAttribute('onclick', 'calc()')
+
 
 
 //입력 기본 기능
@@ -35,6 +42,17 @@ function append(inputValue) {
     let prevInputValue = getText()
     setText(prevInputValue + inputValue)
 }
+function dot(){
+    if(!hasDots[length]){
+        append('.')
+        hasDots[length]=true;
+    }
+}
+function calc(){
+    let prevInputValue = getText()
+    if(prevInputValue=='119.') call119()
+    setText(eval(prevInputValue)==undefined?'':eval(getText()))
+}
 
 //한 글자 지우기 기능
 function erase(){
@@ -45,6 +63,11 @@ function erase(){
 //메모리 기능
 function recall() {setText(memoryValue)}
 function memory() {memoryValue = getText()}
+
+//신고 기능
+function call119(){
+    window.location.href='http://119.go.kr/Center119/regist.do?certify=R';
+}
 
 
 
