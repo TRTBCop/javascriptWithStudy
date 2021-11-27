86c53a5f6a0a0b4fa7c13f36c11e0c9c6346d716
윤예은 <60867063+yeeun-yun97@users.noreply.github.com>
2021-11-27 23:46:07 +0900

    w05 erase button

--- Yoon_Yeeun/[5주차] 계산기/script.js
+++ Yoon_Yeeun/[5주차] 계산기/script.js
@@ -7,6 +7,7 @@ const RECALL_BUTTON = document.querySelector("div form input.memory[value='R']")
 let memoryValue = ''
 
 const CLEAR_BUTTON = document.querySelector("div form input[type=button][value='C']")
+const CLEAR_ONE_BUTTON = document.querySelector("div form input[type=button].clear.one")
 
 clear()
 
@@ -20,29 +21,32 @@ for (let i = 0; i < INPUT_NUMBERS.length; i++) {
 MEMORY_BUTTON.setAttribute('onclick', 'memory()')
 RECALL_BUTTON.setAttribute('onclick', 'recall()')
 CLEAR_BUTTON.setAttribute('onclick', 'clear()')
+CLEAR_ONE_BUTTON.setAttribute('onclick', 'erase()')
 
 
 //입력 기본 기능
-function getText() {
-    return INPUT_WINDOW.getAttribute("value")
-}
-
-function setText(text) {
-    INPUT_WINDOW.setAttribute("value", text)
-}
-
+function getText() {return INPUT_WINDOW.getAttribute("value")}
+function setText(text) {INPUT_WINDOW.setAttribute("value", text)}
 function clear() {
     setText('')
     console.log('clear')
 }
-
-function recall() {setText(memoryValue)}
-function memory() {memoryValue = getText()}
-
 function append(inputValue) {
     let prevInputValue = getText()
     setText(prevInputValue + inputValue)
 }
+
+//한 글자 지우기 기능
+function erase(){
+    let prevInputValue = getText()
+    setText(prevInputValue.substr(0,prevInputValue.length-1))
+}
+
+//메모리 기능
+function recall() {setText(memoryValue)}
+function memory() {memoryValue = getText()}
+
+
 
 
 
