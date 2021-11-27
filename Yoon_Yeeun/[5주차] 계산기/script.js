dbf390a1afebfd25abf150a47176aef1789f732f
윤예은 <60867063+yeeun-yun97@users.noreply.github.com>
2021-11-27 23:37:08 +0900

    w05 M and R button

---
+++ Yoon_Yeeun/[5주차] 계산기/script.js
@@ -0,0 +1,57 @@
+const INPUT_WINDOW = document.querySelector("div form input[type=text]")
+const INPUT_NUMBERS = document.querySelectorAll("div form input[type=button].number")
+const INPUTS = document.querySelectorAll("div form input[type=button]")
+
+const MEMORY_BUTTON = document.querySelector("div form input.memory[value='M']")
+const RECALL_BUTTON = document.querySelector("div form input.memory[value='R']")
+let memoryValue = ''
+
+const CLEAR_BUTTON = document.querySelector("div form input[type=button][value='C']")
+
+clear()
+
+//숫자 버튼 클릭 이벤트 설정.
+for (let i = 0; i < INPUT_NUMBERS.length; i++) {
+    let input = INPUT_NUMBERS[i];
+    let inputValue = input.getAttribute("value")
+    input.setAttribute('onclick', `append("${inputValue}")`)
+}
+
+MEMORY_BUTTON.setAttribute('onclick', 'memory()')
+RECALL_BUTTON.setAttribute('onclick', 'recall()')
+CLEAR_BUTTON.setAttribute('onclick', 'clear()')
+
+
+//입력 기본 기능
+function getText() {
+    return INPUT_WINDOW.getAttribute("value")
+}
+
+function setText(text) {
+    INPUT_WINDOW.setAttribute("value", text)
+}
+
+function clear() {
+    setText('')
+    console.log('clear')
+}
+
+function recall() {setText(memoryValue)}
+function memory() {memoryValue = getText()}
+
+function append(inputValue) {
+    let prevInputValue = getText()
+    setText(prevInputValue + inputValue)
+}
+
+
+
+
+
+
+
+
+
+
+
+
