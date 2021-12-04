2c2e4997483b1b3696df3beed6b2322fc5c84fbb
윤예은 <60867063+yeeun-yun97@users.noreply.github.com>
2021-12-04 21:51:29 +0900

    w06 animation add mouth and eye

---
+++ Yun_Yeeun/[6주차] 유령/script.js
@@ -0,0 +1,8 @@
+
+
+
+document.body.addEventListener("mousemove", function (e) {
+    console.log(e)
+    document.querySelector('.ghost').style.left = e.pageX + 'px'
+    document.querySelector('.ghost').style.top = e.pageY + 'px'
+})
