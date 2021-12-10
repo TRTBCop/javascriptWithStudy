23ea56db5c92fcbe8093d5403d773fff56f29735
kimsj912 <aa991122aa@gmail.comn>
2021-12-10 17:57:45 +0900

    img adding

--- Kim_Sujeong/3D큐브(JS)/script.js
+++ Kim_Sujeong/3D큐브(JS)/script.js
@@ -1,5 +1,5 @@
 let deg = 0;
 setInterval(()=>{
     deg=deg-1;
-    document.querySelector(".cube").style.transform = `rotateX(${deg}deg)`
+    document.querySelector(".cube").style.transform = `rotateX(${deg}deg) rotateY(60deg)`
 },10);
